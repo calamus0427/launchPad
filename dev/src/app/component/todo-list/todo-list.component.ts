5a1f925e6f8c7e1400a08d790fcee19b937b1596
calamus0427 <1211994507@qq.com>
2017-11-01 15:19:23 +0800

    增加删除全部已完成项和未完成项

--- dev/src/app/component/todo-list/todo-list.component.ts
+++ dev/src/app/component/todo-list/todo-list.component.ts
@@ -20,7 +20,6 @@ export class TodoListComponent implements OnInit {
   constructor(private storage: StorageService) {
     this.list = [] ;
     this.things = '' ;
-    console.log(this.storage);
    }
 
   ngOnInit() {
@@ -38,7 +37,6 @@ export class TodoListComponent implements OnInit {
       'name': this.things ,
       'status': 0,
     };
-    console.log(todolist);
     if (todolist) {
       todolist.push(obj);
       this.storage.setItem('todolist', todolist);
@@ -59,7 +57,6 @@ export class TodoListComponent implements OnInit {
           'name': this.things ,
           'status': 0,
         };
-        console.log(todolist);
         if (todolist) {
           this.storage.setItem('todolist', todolist);
           todolist.push(obj);
@@ -79,9 +76,40 @@ export class TodoListComponent implements OnInit {
     var todolist = this.storage.getItem('todolist');
     this.list.splice(key,1);
     this.storage.setItem('todolist', this.list);
-    console.log(todolist);
     // 这个只能删除整个项，不能删除字符串里的具体内容
     // this.storage.removeItem('todolist[0]');
+  }
+
+  //这个方法有问题
+  delAll(flag){
+    if(flag === 0){
+      // debugger
+      var todolist = this.storage.getItem('todolist');
+      var len = this.list.length ;
+      console.log(this.list,len);
+      for(var i = 0 ;i < len  ;i++){
+          if(this.list[i].status === 0){
+            this.list.splice(i,1);
+            i--;   //注意：因为splice删除数组以后会修改数组的坐标值所以必须i--修改坐标才能删除全部数据
+            this.storage.setItem('todolist', this.list);
+          }
+      }
+      console.log(this.list);
+      // this.storage.setItem('todolist', this.list);
+    }else if(flag === 1){
+      var todolist = this.storage.getItem('todolist');
+      var len = this.list.length ;
+      console.log(this.list,len);
+      for(var i = 0 ;i < len ;i++){
+        if(this.list[i].status === 1){
+          this.list.splice(i,1);
+          i--;   //注意：因为splice删除数组以后会修改数组的坐标值所以必须i--修改坐标才能删除全部数据
+          this.storage.setItem('todolist', this.list);
+        }
+      }
+
+    }
+
   }
 
   clearAll(flag){
@@ -92,7 +120,6 @@ export class TodoListComponent implements OnInit {
   }
 
   changeStatus(key){
-    console.log(this.list[key].status) ;
     this.list[key].status = (this.list[key].status === 0?1:0);
     this.storage.setItem('todolist', this.list);
   }
