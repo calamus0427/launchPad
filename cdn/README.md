b3ee51dcc2b2b12e31f1dd2a1105d4879d52a207
calamus0427 <1211994507@qq.com>
2019-04-26 11:12:00 +0800

    all

--- cdn/README.md
+++
@@ -1,2 +0,0 @@
-# angularJS 实例
-**angularJS版本** ：  1.5.8
--- cdn/js/index.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('userCtrl', function($scope) {
-    $scope.fName= "John";
-    $scope.lName= "Doe";
-});
